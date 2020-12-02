92d47ddf88e56bb380039fcce38fe11942e4df08
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-12-02 15:14:56 -0300

    Métodos HTTP

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,27 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello Alex" });
+/* Métodos HTTP:
+GET: Buscar informações do Back-end
+POST: Criar um informação no Back-end
+PUT/PATCH: Alterar uma informação no back-end
+DELETE: Deletar uma informação no back-end
+*/
+
+app.get("/projects", (request, response) => {
+  return response.json(["Projeto 1", "Projeto2"]);
+});
+
+app.post("/projects", (request, response) => {
+  return response.json(["Projeto 1", "Projeto2", "Projeto3"]);
+});
+
+app.put("/projects/:id", (request, response) => {
+  return response.json(["Projeto 5", "Projeto2", "Projeto3"]);
+});
+
+app.delete("/projects/:id", (request, response) => {
+  return response.json(["Projeto2", "Projeto3"]);
 });
 
 app.listen(3333, () => {
