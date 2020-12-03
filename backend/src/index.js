fd56786c4d8a4bd8c5e3a24d15b57f358b6e8ca4
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-12-03 10:34:10 -0300

    Tipos de Parametros

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 /* Métodos HTTP:
 GET: Buscar informações do Back-end
 POST: Criar um informação no Back-end
@@ -9,15 +11,32 @@ PUT/PATCH: Alterar uma informação no back-end
 DELETE: Deletar uma informação no back-end
 */
 
+/* Tipo de Parâmetros
+Query Params: Filtros e paginação
+Route Params: Identificar recursos (Atualizar/Deletar)
+Request Params: Conteúdo na hora de criar ou editar um recurso (JSON)
+*/
+
 app.get("/projects", (request, response) => {
+  const { title, owner } = request.query;
+  console.log(title);
+  console.log(owner);
+
   return response.json(["Projeto 1", "Projeto2"]);
 });
 
 app.post("/projects", (request, response) => {
+  const body = request.body;
+  console.log(body);
+
   return response.json(["Projeto 1", "Projeto2", "Projeto3"]);
 });
 
 app.put("/projects/:id", (request, response) => {
+  const params = request.params;
+
+  console.log(params);
+
   return response.json(["Projeto 5", "Projeto2", "Projeto3"]);
 });
 
