144468dd0286e5d06c104c0b1e533a939d52fa87
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-12-03 11:55:06 -0300

    Middlewares

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,24 +1,54 @@
 const express = require("express");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 
 /* Métodos HTTP:
-GET: Buscar informações do Back-end
-POST: Criar um informação no Back-end
-PUT/PATCH: Alterar uma informação no back-end
-DELETE: Deletar uma informação no back-end
+  GET: Buscar informações do Back-end
+  POST: Criar um informação no Back-end
+  PUT/PATCH: Alterar uma informação no back-end
+  DELETE: Deletar uma informação no back-end
 */
 
-/* Tipo de Parâmetros
-Query Params: Filtros e paginação
-Route Params: Identificar recursos (Atualizar/Deletar)
-Request Params: Conteúdo na hora de criar ou editar um recurso (JSON)
+/* Tipo de Parâmetros:
+  Query Params: Filtros e paginação
+  Route Params: Identificar recursos (Atualizar/Deletar)
+  Request Params: Conteúdo na hora de criar ou editar um recurso (JSON)
+*/
+
+/* Middleware:
+  Interceptador de requisições que pode interromper totalmente a requisição ou alterar dados da requisição.
+
 */
 
 const projects = [];
+
+function logRequests(request, response, next) {
+  const { method, url } = request;
+  const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+  console.log(logLabel);
+  console.time(logLabel);
+
+  next(); // Próximo Middleware
+
+  console.timeEnd(logLabel);
+}
+
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if (!isUuid(id)) {
+    return response.status(400).json({ error: "Inválid project ID." });
+  }
+
+  return next();
+}
+
+app.use(logRequests);
+app.use("/projects/:id", validateProjectId);
 
 app.get("/projects", (request, response) => {
   const { title } = request.query;
