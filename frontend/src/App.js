df53ba7a26791c3b0cbcca29000a4a6a3144d696
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-12-18 10:30:20 -0300

    Estado e Imutabilidade

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,32 +2,46 @@
 Colocar sempre com letra maiuscula no começo.
 ex: App.js
 */
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 
 /* 3 Conceitos importantes do React.
- * Componente:
- * Propriedade:
- * Estado:
+ * Componente
+ * Propriedade
+ * Estado e Imutabilidade
  */
 
 function App() {
+  const [projects, setProjects] = useState([
+    "Desenvolvimento de app",
+    "Front-End web",
+  ]);
+
+  /* useState retorna um array com 2 posições.
+   * 1. Variável com o seu valor inicial.
+   * 2. Função para atualizarmos esse valor.
+   */
+
+  function handleAddProject() {
+    //  projects.push(`Novo Projeto ${Date.now()}`);
+    setProjects([...projects, `Novo Projeto ${Date.now()}`]); // utilizando o conceito de imutabilidade
+
+    console.log(projects);
+  }
+
   return (
     // Utilizando o conceito de fragment <>  </>
     <>
-      <Header title="HomePage">
-        <ul>
-          <li>HomePage</li>
-          <li>Projects</li>
-        </ul>
-      </Header>
-      <Header title="Projects">
-        <ul>
-          <li>HomePage</li>
-          <li>Projects</li>
-          <li>Login</li>
-        </ul>
-      </Header>
+      <Header title="Projects" />
+
+      <ul>
+        {projects.map((project) => (
+          <li key={project}>{project}</li>
+        ))}
+      </ul>
+      <button type="button" onClick={handleAddProject}>
+        Adicionar projeto
+      </button>
     </>
   );
 }
