9cd3044f6172bfcd22b2175ab97e541f576bb458
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2021-01-26 15:21:26 -0300

    Primeiro Projeto Node - ESLint

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -2,10 +2,10 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (request, response)=> {
-  return response.json({message: 'Alex Barbosa'})
+app.get('/', (request, response) => {
+  return response.json({ message: 'Alex Barbosa' });
 })
 
-app.listen(3333, ()=> {
-  console.log('Server started on port 3333!')
+app.listen(3333, () => {
+  console.log('Server started on port 3333!');
 })
