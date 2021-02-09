56502ae01f18b9e50ac6b36f9b44189433218736
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2021-02-09 11:12:24 -0300

    BackEnd Usuarios - Relacionamento nos Models

--- projeto-backend/src/database/migrations/1612288626920-CreateAppointments.ts
+++ projeto-backend/src/database/migrations/1612288626920-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1612288626920
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- projeto-backend/src/database/migrations/1612875959007-CreateUsers.ts
+++ projeto-backend/src/database/migrations/1612875959007-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1612875959007 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
