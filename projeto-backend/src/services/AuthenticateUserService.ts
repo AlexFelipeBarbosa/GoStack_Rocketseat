c47eeafb82410378e2c1514d6216bddec849f8e3
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2021-02-11 10:22:16 -0300

    BackEnd Autenticacao - Gerando Token JWT

--- projeto-backend/src/services/AuthenticateUserService.ts
+++ projeto-backend/src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -32,7 +34,12 @@ class AuthenticateUserService {
     }
 
     // Usuario autenticado
-    return { user };
+    const token = sign({}, 'aa2481f29d92155e8ffdb63a9f8ff9fa', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return { user, token };
   }
 }
 
