38f71f25b7b17b241a05441e9bacaaf92dd8b9e3
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2021-03-22 11:34:49 -0300

    React Primeiro Projeto - Estilizando Detalhe

--- primeiro-projeto-react/src/pages/Repository/index.tsx
+++ primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -7,7 +11,52 @@ interface RepositoryParams {
 
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
-  return <h1>Repository:{params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/" />
+        <FiChevronLeft size={16} />
+        Voltar
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+            alt="RocketSeat"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>descricao do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="sdadada">
+          <div>
+            <strong>asdasdadd</strong>
+            <p>asdadadaddad</p>
+          </div>
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
